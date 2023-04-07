16df8a66f02118894f5eb9dded44cfe3c37a71ae
ConnnorS <117893558+ConnnorS@users.noreply.github.com>
2023-04-07 13:33:30 +1000

    commit

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,3 +1,17 @@
+import React, {useState, useEffect} from 'react';
+
+function pullData() {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        fetch('/moviesSearch.json')
+        .then(response => response.json())
+        .then(movies => setMovies(movies));
+    }, [])
+}
+
+
+
 export default function Movies() {
     return (
         <div>
