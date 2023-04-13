81c00b41e77dfcc768404d84a3b8f49ae86af981
ConnnorS <connorsouth117@protonmail.com>
2023-04-13 12:48:56 +1000

    More info data updated

--- src/pages/MoreInfo.js
+++ src/pages/MoreInfo.js
@@ -1,15 +1,38 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { useSearchParams } from "react-router-dom"
 
 export default function MoreInfo() {
+    // useState variable for more in-depth movie information
+    const [mainInfo, setMainInfo] = useState([]);
+    const [formattedInfo, setFormattedInfo] = useState([]);
+
     // get the search parameters (imdbID)
     const [searchParams] = useSearchParams();
     const imdbID = searchParams.get("imdbID");
 
+    // fetch the API info
+    const url = `http://sefdb02.qut.edu.au:3000/movies/data/${imdbID}`;
+    useEffect(() => {
+        fetch(url)
+            .then(response => response.json())
+            .then(mainInfo => setMainInfo(mainInfo));
+    }, []);
+
     return (
     <div>
-        <h1>More Info</h1>
-        <h2>Your ID is {imdbID}</h2>
+        <h1>{mainInfo.title}</h1>
+
+        <p>
+            Released in: {mainInfo.year}<br/>
+            Run time: {mainInfo.runtime} minutes<br/>
+            Genres: {mainInfo.genres}<br/>
+            Country: {mainInfo.country}<br/>
+            Box Office: ${mainInfo.boxoffice}<br/>
+            <br/>
+            <i>{mainInfo.plot}</i><br/>
+            <br/>
+            <img src = {mainInfo.poster}/>
+        </p>
     </div>
     )
 }
